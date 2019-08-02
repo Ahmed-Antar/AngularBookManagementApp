4520b3d3181c3a8c38f418928006fdcde69aad59
Antar Ahmed <aantar@SQLI3075.SQLI>
2019-08-02 15:57:19 +0200

    Feat: Configure firebase and authentication methods.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularBookManagementApp';
+  constructor(){
+
+    const config = {
+      apiKey: "AIzaSyC82b3p6GjkkB0E2RfXY9gELbRACkqwYKw",
+      authDomain: "bookmanagementng.firebaseapp.com",
+      databaseURL: "https://bookmanagementng.firebaseio.com",
+      projectId: "bookmanagementng",
+      storageBucket: "",
+      messagingSenderId: "693124580618",
+      appId: "1:693124580618:web:9807b22c062d92ab"
+    };
+    firebase.initializeApp(config);
+
+  }
 }
